607618866403d4bec8ecb9108d067cf319efbf6c
sadmonad <sadmonad@gmail.com>
2018-09-07 17:28:30 +0300

    Fix elasticsearch config

--- elastic_config.py
+++ elastic_config.py
@@ -1,5 +1,5 @@
 from elasticsearch import Elasticsearch
 
-elastic_client = Elasticsearch(['http://52.15.110.231:16384'])
+elastic_client = Elasticsearch(['http://localhost:16384'])
 INDEX_NAME = 'stream'
 DOC_TYPE = 'stream'
